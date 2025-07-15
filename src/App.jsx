6aad10f6ae4bef486caefd9b3cda68f5fcd4989f
brench maina <bein67544@gmail.com>
2025-07-15 15:06:54 +0300

    core delivarables done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import Header from "./components/Header";
+import PlantPage from "./components/PlantPage";
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <PlantPage />
+    </div>
+  );
+} 
+
+export default App;
