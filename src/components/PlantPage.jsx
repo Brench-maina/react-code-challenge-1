1bb7dd7870b6d2ec28a013db2f2b97be89c30f2e
brench maina <bein67544@gmail.com>
2025-07-16 09:51:55 +0300

    modified

--- src/components/PlantPage.jsx
+++ src/components/PlantPage.jsx
@@ -4,16 +4,21 @@ import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+    //state for all plants
     const [plants,setPlants] = useState([]);
+    // state for search input
     const [search,setSearch] =useState("");
 
-    useEffect(() => {
+    //Fetch plants from server on component 
+
+     useEffect(() => {
         fetch("http://localhost:6001/plants")
         .then((res) =>res.json())
         .then((data) => setPlants(data))
         .catch((err) => console.error("Failed to fetch plants:",err));
     }, []);
 
+      //Handles adding a new plant with POST method
     const handleAddPlant = (newPlant) => {
         fetch("http://localhost:6001/plants", {
             method:"POST",
@@ -29,6 +34,7 @@ function PlantPage() {
         .catch((err) => console.error("Failed to add plant:", err))
         };
 
+        //Handles toggling soldOut status with PATCH method
         const handleToggleSoldOut = (id,newStatus) =>{
             fetch(`http://localhost:6001/plants/${id}`,{
              method: "PATCH",
@@ -45,18 +51,19 @@ function PlantPage() {
                 setPlants(updatedPlants);
             })
         }
-
+// delete plants with DELETE method
         const handleDeletePlant = (id) => {
             fetch(`http://localhost:6001/plants/${id}`, {
                 method: "DELETE"
             })
             .then(() => {
+
                 const updatePlants = plants.filter((plant) => plant.id !== id);
                 setPlants(updatePlants);
             })
             .catch((err) => console.error("Failed to delete plant,",err));
            };
-
+   // Filters plants based on search input
            const filteredPlants = plants.filter((plant) => 
          plant.name && plant.name.toLowerCase().includes(search.toLowerCase())
         );
